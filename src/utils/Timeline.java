8a9428bd4eca04441258e7783f1c12a4ef7b3641
Serhii Tymoshenko <sergei.timoshenko@pm.me>
2023-10-21 17:16:18 +0200

    Initial commit

---
+++ src/utils/Timeline.java
@@ -0,0 +1,10 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+public class Timeline {
+    static ScheduledExecutorService timeline = Executors.newSingleThreadScheduledExecutor();
+
+    public void run() {
+        
+    }
+}
